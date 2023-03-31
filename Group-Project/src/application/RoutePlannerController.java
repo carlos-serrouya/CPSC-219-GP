8570393f53972fdcd6af612c9177d0de77e126c9
CS219-user <CS219-user@10.0.0.39>
2023-03-31 21:54:04 +0000

    update the scene to have a button, new map as well

--- Group-Project/src/application/RoutePlannerController.java
+++ Group-Project/src/application/RoutePlannerController.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import javafx.geometry.Point2D;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -12,98 +11,148 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.input.KeyEvent;
-
+/** 
+ * This controller class takes the 4 locations the user inputs as well as a max length. it then outputs the most efficient path under the maximum length input by the user.
+ * @author Romeo Champagne
+ *
+ */
 public class RoutePlannerController {
 	Stage testStage;
 	
 	@FXML
 	private Label clickLocationLabel;
+	/**
+	 * 2D array of integers that represent the locations where the user has clicked on the scene
+	 * The first dimension represents the click event number, and the second dimension
+	 * represents the x and y coordinates of the click event.
+	 */
 	private int[][] clickLocations = new int[3][4];
+	
+	
+	/**
+	 * tracks how many clicks have happened so far
+	 */
 	private int clickCounter = 0;
+	
+	/**
+	 * max length that the user entered
+	 */
 	@FXML
 	private TextField max;
+	
+	/**
+	 * confirms to the user what they have entered in the scene for max length
+	 */
 	@FXML
 	private Label maxLengthNumber;
+	
+	
+	/**
+	 * This method is called when the "Find Path" button is clicked on the GUI.
+	 * It gets the x and y values of the clicks from clickLocations and 
+	 * the maximum length entered. It then proceeds to find the most efficient path
+	 * that connects the clicks without exceeding the maximum length.
+	 */
+	@FXML
+	public void FindPathButtonClick() {
+		// gets the x and y values from clickLocations
+		 int[] XvalMain = clickLocations[0]; 
+		 int[] YvalMain = clickLocations[1];
+		 
+		 // getting the double value of the textField entered
+		 double maxLengthMain = Double.parseDouble(max.getText());
+			
+		
+		GetMagnitudes start = new GetMagnitudes();
+		start.setXval(XvalMain);//so we can use private
+		start.setYval(YvalMain);//parameters
+		double [] magnitudesMain = GetMagnitudes.pythag();
+
+		GetSetOfLists sets = new GetSetOfLists();
+		sets.magnitudes = magnitudesMain;
+		sets.maxLength = maxLengthMain;
+		String [][] setOfListsMain = GetSetOfLists.filter();
+
+
+		PathConnect con = new PathConnect();
+		con.setOfLists = setOfListsMain;
+		String [][]finalSetOfListsMain = PathConnect.connect();
+
+		GetSetOfLengths connected = new GetSetOfLengths();
+		connected.magnitudes = magnitudesMain;
+		connected.setOfLists = finalSetOfListsMain;
+		double[][] finalSetOfLengthsMain = GetSetOfLengths.filterlengths();
+
+		EfficiencyTest test = new EfficiencyTest();
+		test.setOfLists = finalSetOfListsMain;
+		test.setOfLengths = finalSetOfLengthsMain;
+		String[] bestPathsMain = EfficiencyTest.best();
+		System.out.println(Arrays.toString(bestPathsMain));
+		
+		String stringBestPaths = "";
+		for (int i=0 ; i<bestPathsMain.length; i++){
+			stringBestPaths += bestPathsMain[i]+ " ";
+		}
+		Scene displayScene = new Scene(new Label (stringBestPaths));
+		testStage.setScene(displayScene);
+		//displays second scene the set of best paths
+    }
+
+	/**
+	 * the next 2 methods work together. They are mostly for readibility but also add some complexity. 
+	 * The first method sets the number beside the max length input box as 0
+	 * When a key is typed in the box, the MaxKeyTyped method will e called.
+	 */
 
 	@FXML
 	public void initialize() {
+		
 		  maxLengthNumber.setText("0");
-	       max.setOnKeyTyped(this::handleMaxKeyTyped);
+	       max.setOnKeyTyped(this::MaxKeyTyped);
 	}
+	
+	/**
+	 * When key is typed in the max length box, this method is called. 
+	 * It will update the wording beside the maxLength input box to show what the user has input.
+	 * 
+	 */
 	@FXML
-	private void handleMaxKeyTyped(KeyEvent event) { // updates code in scene to see max length. hoping to use this later for  methods that need it
+	private void MaxKeyTyped(KeyEvent event) { // updates code in scene to see max length
+		
 	    String maxText = max.getText();
 	    int maxLength = maxText.length();
 	    char typedChar = event.getCharacter().charAt(0);
+	    // has to be a digit to get updated
 	    if (Character.isDigit(typedChar)) {
-	        maxLengthNumber.setText(String.valueOf(maxLength));
-	        //double max1 = Double.parseDouble(maxLengthNumber.getText()); // set max1 to the same value as maxLength
+	        maxLengthNumber.setText(maxText);
+	        
 	    }
 	}
+	/**
+	 * The method is important. It is called when the user clicks on the map on the first scene.
+	 * As the user selects the location of their points on the map, the method is 
+	 * called an will update the clickLocations array. 
+	 * It does this until it has counted 4 clicks by the user.
+	 * once 4 clicks are recorded it will allow the rest of the code to function
+	 * 
+	 */
 
 	@FXML
-	void handleMouseClick(MouseEvent event) {
-	    //int[][] clickLocations = new int[3][4]; // initialize 2D array to store user clicks
-	    // form of the array: the first row contains the X values,
-	    // the second row contains the Y values
-	    // the third row contains the maximum value.
+	void MouseClick(MouseEvent event) {
 	    
 	    double x = event.getX();
 	    double y = event.getY();
-
+	    // records the locations of the dots in an x and y axis
 	    if (clickCounter < 4) {
 	        clickLocations[0][clickCounter] = (int) x;
 	        clickLocations[1][clickCounter] = (int) y;
 	        clickCounter++;
 	    }
-
+	    // after the user has clicked 4 times this will run
 	    if (clickCounter == 4) {
 	        double max1 = Double.parseDouble(maxLengthNumber.getText()); // set max1 to the same value as maxLength
 	        clickLocations[2][0] = (int) max1;
-	        // perform calculations using the clickLocations array
-	    
-	    int[] XvalMain = clickLocations[0]; 
-		int[] YvalMain = clickLocations[1];
-	    
-		double maxLengthMain = clickLocations[2][0];
-	    
-	    
-			//defines arrays well need later: note we did i like this so 
-			// so we can eventually do all these operations in a different class
-
-			GetMagnitudes start = new GetMagnitudes();
-			start.setXval(XvalMain);//so we can use private
-			start.setYval(YvalMain);//parameters
-			double [] magnitudesMain = GetMagnitudes.pythag();
-
-			GetSetOfLists sets = new GetSetOfLists();
-			sets.magnitudes = magnitudesMain;
-			sets.maxLength = maxLengthMain;
-			String [][] setOfListsMain = GetSetOfLists.filter();
-
-
-			PathConnect con = new PathConnect();
-			con.setOfLists = setOfListsMain;
-			String [][]finalSetOfListsMain = PathConnect.connect();
-
-			GetSetOfLengths connected = new GetSetOfLengths();
-			connected.magnitudes = magnitudesMain;
-			connected.setOfLists = finalSetOfListsMain;
-			double[][] finalSetOfLengthsMain = GetSetOfLengths.filterlengths();
-
-			EfficiencyTest test = new EfficiencyTest();
-			test.setOfLists = finalSetOfListsMain;
-			test.setOfLengths = finalSetOfLengthsMain;
-			String[] bestPathsMain = EfficiencyTest.best();
-			System.out.println(Arrays.toString(bestPathsMain));
-			
-			String stringBestPaths = "";
-			for (int i=0 ; i<bestPathsMain.length; i++){
-				stringBestPaths += bestPathsMain[i]+ " ";
-			}
-			Scene displayScene = new Scene(new Label (stringBestPaths));
-			testStage.setScene(displayScene);
-			//displays second scene the set of best paths
 	    }
-	}
+		
+	}   
 }
